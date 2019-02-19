ead4db098ffa58a87bb373e107cafd37aa0ef911
aallenchen2018 <42566084+aallenchen2018@users.noreply.github.com>
2019-02-19 09:24:46 +0800

    字符串

---
+++ 字符串反转.py
@@ -0,0 +1,60 @@
+#列表
+
+s='apple12345'
+'''
+l=list(s)
+l.reverse()
+print(''.join(l))
+
+#步长法
+
+b=s[::-1]
+print(b)
+
+#d递归
+
+def func(s):
+    if len(s)<=1:
+        return s
+    else:
+        return func(s[1:])+s[0]
+result=func(s)
+print(result)
+
+#for index
+
+def func(s):
+    result=''
+    max_index=len(s)-1
+    for index,value in enumerate(s):
+        result+=s[max_index-index]
+    return result
+re=func(s)
+print(re)
+
+'''
+l=list(s)
+l.reverse()
+re=''.join(l)
+print(re)
+
+re=s[::-1]
+print(re)
+
+def func(s):
+    max_index=len(s)-1
+    result=''
+    for index,value in enumerate(s):
+        result+=s[max_index-index]
+    return result
+re=func(s)  
+print(re)
+
+def func(s):
+    if len(s)<1:
+        return s
+    else:
+        return s[-1]+func(s[:-1])
+re=func(s)
+print(re)
+
